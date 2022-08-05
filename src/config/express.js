55f5839d8480990c60bf4ed0f791aac3d84c82fe
Nícalo Ribeiro <nicalo63@gmail.com>
2022-08-05 13:31:48 -0300

    Utilizacao das rotas de usuarios

--- src/config/express.js
+++ src/config/express.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import helloWorldRouter from '../routes/hello-worldRoutes.js';
+import usuariosRouter from '../routes/usuariosRoutes.js';
 
 const app = express();
 const BASE_PATH = '/api';
@@ -11,5 +12,6 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use(BASE_PATH, helloWorldRouter);
+app.use(BASE_PATH, usuariosRouter);
 
 export default app;
