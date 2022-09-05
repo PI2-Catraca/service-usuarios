68c58d8fefee384ab04d77ac00234d07ae516c68
lbrunofidelis <l.brunofidelis@gmail.com>
2022-09-05 02:37:41 -0300

    [WIP] Login

--- src/routes/acessoRoutes.js
+++ src/routes/acessoRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 const acessosRouter = express.Router();
 
-import { getAllAcessos, getAcessoByCpf } from '../controller/acessoController.js';
+import { getAllAcessos, getAcessoByCpf, getAcessoByIdCatraca } from '../controller/acessoController.js';
 
 acessosRouter.get('/acesso/todos', getAllAcessos);
 acessosRouter.get('/acesso/cpf/:cpf', getAcessoByCpf);
+acessosRouter.get('/acesso/idCatraca/:id', getAcessoByIdCatraca);
 
 export default acessosRouter;
--- src/routes/usuariosRoutes.js
+++ src/routes/usuariosRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 const usuariosRouter = express.Router();
 
-import { postUsuario, getUsuarioByCpf, getAllUsuarios } from '../controller/usuarioController.js';
+import { postUsuario, getUsuarioByCpf, getAllUsuarios, login } from '../controller/usuarioController.js';
 
 usuariosRouter.post('/usuario/novo', postUsuario);
 usuariosRouter.get('/usuario/todos', getAllUsuarios);
 usuariosRouter.get('/usuario/cpf/:cpf', getUsuarioByCpf);
+usuariosRouter.post('/usuario/login', login);
 
 export default usuariosRouter;
