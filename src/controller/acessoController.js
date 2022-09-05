8986cd473a7f4576a8f63697e164a0587d8a6371
lbrunofidelis <l.brunofidelis@gmail.com>
2022-09-04 21:36:05 -0300

    Busca de acesso por CPF

--- src/controller/acessoController.js
+++ src/controller/acessoController.js
@@ -5,6 +5,11 @@ const getAllAcessosQuery = async () => {
     return rows;
 }
 
+const getAcessosByCpfQuery = async (cpf) => { 
+    const { rows } = await Database.query(`SELECT * FROM tb_user_catraca WHERE usuario_cpf = $1`, [cpf]);
+    return rows;
+}
+
 export const getAllAcessos = async (req, res) => {
     try {
         const acessos = await getAllAcessosQuery();
@@ -28,6 +33,32 @@ export const getAllAcessos = async (req, res) => {
     } catch (error) {
         return res.status(400).send({
             message: "Houve um erro ao recuperar os acessos.",
+            data: {
+                error
+            }
+        });
+    }
+}
+
+export const getAcessoByCpf = async (req, res) => {
+    const { cpf } = req.params;
+
+    try {
+        const acessos = await getAcessosByCpfQuery(cpf);
+        console.log('acessos:', acesso);
+
+        if(acessos.length > 0) {
+            return res.status(200).send({
+                message: "Acessos do CPF recuperados com sucesso.",
+                data: {
+                    acessos
+                }
+            });
+        }
+
+    } catch (error) {
+        return res.status(400).send({
+            message: "Houve um erro ao recuperar os acessos deste CPF",
             data: {
                 error
             }
