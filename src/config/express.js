aeb34f406c36a9782850ef4013759ddfa34453d2
itallogravina <itallo.gravina@gmail.com>
2022-09-12 21:25:58 -0300

    cors erro

--- src/config/express.js
+++ src/config/express.js
@@ -8,9 +8,13 @@ import acessosRouter from '../routes/acessoRoutes.js';
 
 const app = express();
 const BASE_PATH = '/api';
-
+const urlOrigin = 'http://localhost:3000'
+let corsOptions = {
+  origin: urlOrigin,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 app.set('port', process.env.PORT || 5000);
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json({limit: '50mb', type: 'application/json'}));
 app.use(BASE_PATH, helloWorldRouter);
 app.use(BASE_PATH, usuariosRouter);
